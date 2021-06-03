e2aa261898f4717f2396cad2b429591857bd3b75
Ren Komatsu <komatsu@robot.t.u-tokyo.ac.jp>
2021-06-03 11:06:36 +0900

    Add more features in simulator

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
    'sphinx.ext.viewcode'
 ]
 napoleon_google_docstring = False
+autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- simulator.py
+++ simulator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from dataclasses import dataclass
 from scipy.spatial import distance
 
@@ -13,12 +13,12 @@ class Source:
 
     Examples
     --------
-    >>> Source(np.array([1, 2, 3]), 2.0)
-    Source(loc=array([1, 2, 3]), intensity=2.0)
+    >>> Source([1, 2, 3], 2.0)
+    Source(loc=[1, 2, 3], intensity=2.0)
     """
 
-    #: Location of the radiation source such that np.array([x, y, z]) [m].
-    loc: np.ndarray
+    #: Location of the radiation source such that [x, y, z] [m].
+    loc: List[float]
     #: Intensity of the radiation source [MBq]. (default is 1.0)
     intensity: float = 1.0
 
@@ -31,12 +31,12 @@ class AreaSource:
 
     Examples
     --------
-    >>> AreaSource(np.array([1, 2, 3]), 2.0)
-    AreaSource(loc=array([1, 2, 3]), intensity=2.0)
+    >>> AreaSource([1, 2, 3], 2.0)
+    AreaSource(bl_loc=[1, 2, 3], width=2.0, intensity=1.0, div_num=20)
     """
 
-    #: Location of the bottom left of the radiation source such that np.array([x, y, z]) [m].
-    bl_loc: np.ndarray
+    #: Location of the bottom left of the radiation source such that [x, y, z] [m].
+    bl_loc: List[float]
     #: Width of the square radiation area source [m].
     width: float
     #: Intensity of the radiation source [MBq]. (default is 1.0)
@@ -52,12 +52,12 @@ class Detector:
 
     Examples
     --------
-    >>> Detector(np.array([1, 2, 3]), 2.0)
-    Detector(loc=array([1, 2, 3]), factor=2.0)
+    >>> Detector([1, 2, 3], 2.0)
+    Detector(loc=[1, 2, 3], duration=2.0, factor=5)
     """
 
-    #: Location of the detector such that np.array([x, y, z]) [m].
-    loc: np.ndarray
+    #: Location of the detector such that [x, y, z] [m].
+    loc: List[float]
     #: Measurement time of the detector `t` [s]. (default is 10)
     duration: float = 10
     #: Factor to consider counting efficiency, size, etc. `f` (default is 5)
@@ -74,6 +74,12 @@ class World(object):
 
     def __init__(self):
         self.sources: List[Source] = []
+
+    def __repr__(self) -> str:
+        repr_str = f"{self.__class__}: contains {len(self.sources)} sources.\n"
+        src_str = ",\n ".join([str(it) for it in self.sources])
+        repr_str = f"{repr_str}[\n {src_str}\n]"
+        return repr_str
 
     def add_source(self, source: Source):
         """
@@ -104,7 +110,7 @@ class World(object):
         y_lin = np.linspace(bl[1], bl[1] + source.width, source.div_num)
         x_grid, y_grid = np.meshgrid(x_lin, y_lin)
         for x, y in zip(x_grid.flatten(), y_grid.flatten()):
-            self.add_source(Source(loc=np.array([x, y, bl[2]]), intensity=q))
+            self.add_source(Source(loc=[x, y, bl[2]], intensity=q))
 
     def get_measuments(self, detectors: List[Detector]) -> np.ndarray:
         """
@@ -138,8 +144,7 @@ class World(object):
         srcs = np.stack(srcs)
 
         # intensity
-        q = [it.intensity for it in self.sources]
-        q = np.array(q)
+        q = np.array([it.intensity for it in self.sources])
 
         dist = distance.cdist(locs, srcs)
         assert not np.any(
@@ -158,13 +163,18 @@ class World(object):
         return counts
 
     def visualize_world(
-        self, figsize: Tuple[float, float] = (10, 10), plotsize: float = 10
+        self,
+        detectors: Optional[List[Detector]] = None,
+        figsize: Tuple[float, float] = (8, 8),
+        plotsize: float = 10,
     ) -> plt.Axes:
         """
-        Visualize radiation sources in world.
+        Visualize radiation sources and detectors in world (X-Y plane).
 
         Parameters
         ----------
+        detectors: Optional[List[Detector]]
+            List of detectors to be visualized. (default is None)
         figsize: tuple(float, float)
             figsize for plot.
         plotsize: float
@@ -175,6 +185,22 @@ class World(object):
         ax: plt.Axes
             Matplotlib axes
         """
+
+        # Setup plot area
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111)
+        ax.plot(
+            [plotsize, plotsize, -plotsize, -plotsize],
+            [plotsize, -plotsize, plotsize, -plotsize],
+            "wd",
+        )
+        ax.plot([0], [0], "rd")
+        ax.set_aspect("equal")
+        ax.set_title(
+            "X (horizontal), Y (vertical), Origin (red dot), Detector (x) "
+        )
+
+        # Visualize sources
         x = []
         y = []
         c = []
@@ -183,29 +209,33 @@ class World(object):
             y.append(it.loc[1])
             c.append(it.intensity)
 
-        fig = plt.figure(figsize=figsize)
-        ax = fig.add_subplot(111)
         sc = ax.scatter(x, y, s=100, c=c, cmap=plt.cm.jet)
         _ = fig.colorbar(sc, orientation="horizontal")
-        # area
-        ax.plot(
-            [plotsize, plotsize, -plotsize, -plotsize],
-            [plotsize, -plotsize, plotsize, -plotsize],
-            "wd",
-        )
-        ax.plot([0], [0], "rd")
-        ax.set_aspect("equal")
-        ax.set_title("X (horizontal), Y (Vertical), Origin(Red dot)")
+
+        # Visualize detectors if not None
+        if detectors:
+            x_detec = []
+            y_detec = []
+            for it in detectors:
+                x_detec.append(it.loc[0])
+                y_detec.append(it.loc[1])
+            ax.plot(x_detec, y_detec, "bx")
+
         return ax
 
 
 if __name__ == "__main__":
+    # doctest
+    import doctest
+
+    doctest.testmod()
+
     # Construct world
     world = World()
-    world.add_source(Source(loc=np.array([5, 8, 0]), intensity=1))
-    world.add_source(Source(loc=np.array([-7, -2, 0]), intensity=2.3))
-    world.add_source(Source(loc=np.array([-5.5, -2, 0]), intensity=1.3))
-    world.add_area_source(AreaSource(bl_loc=np.array([5, 0, 0]), width=2.0))
+    world.add_source(Source(loc=[5, 8, 0], intensity=1))
+    world.add_source(Source(loc=[-7, -2, 0], intensity=2.3))
+    world.add_source(Source(loc=[-5.5, -2, 0], intensity=1.3))
+    world.add_area_source(AreaSource(bl_loc=[5, 0, 0], width=2.0))
     ax = world.visualize_world()
     plt.show()
 
@@ -219,7 +249,7 @@ if __name__ == "__main__":
     z = 1.5
     detectors = []
     for x, y in zip(x_grid.flatten(), y_grid.flatten()):
-        detectors.append(Detector(loc=np.array([x, y, z])))
+        detectors.append(Detector(loc=[x, y, z]))
 
     cnts = world.get_measuments(detectors)
     print(f"max_count: {cnts.max()}")
